e23efe072dafe33032a7356009792a393645477d
Oleksiytabachyn <oleksiy.tabachyn@gmail.com>
2026-03-23 21:01:16 +0000

    Update node_2d.gd
    comiting to second branch

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+#testing another branch
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
